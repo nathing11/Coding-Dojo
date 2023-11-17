03e4486a6dba1af361190c5997cea259d8b8783e
nathing11 <amenallahbrahim6@gmail.com>
2023-11-17 20:36:33 +0100

    ll

---
+++ Python/Week 1/Day 4/Practice/Building the Application/BuildingtheApplication.py
@@ -0,0 +1,21 @@
+from flask import Flask 
+app = Flask(__name__)    
+@app.route('/')          
+def hello_world():
+    return 'Hello World!' 
+@app.route('/dojo')          
+def dojo():
+    return 'dojo!' 
+@app.route('/<name>')          
+def name(name):
+    return f"have it say {name}"
+
+
+
+
+
+@app.route('/repeat/<int:nub>/<name>')          
+def numb(nub,name):
+    return nub*name
+if __name__=="__main__":      
+    app.run(debug=True)    
