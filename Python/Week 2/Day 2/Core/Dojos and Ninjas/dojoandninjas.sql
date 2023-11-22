bfa8dedda151376c75611d9c89353759627a4162
nathing11 <amenallahbrahim6@gmail.com>
2023-11-22 18:15:54 +0100

    aa

---
+++ Python/Week 2/Day 2/Core/Dojos and Ninjas/dojoandninjas.sql
@@ -0,0 +1,13 @@
+SELECT * FROM dojos;
+INSERT INTO dojos (name)
+VALUES ("walid"),("khaled"),("lazher");
+DELETE FROM dojos 
+WHERE id=1 or  id=2 or id=3;
+INSERT INTO dojos (name)
+VALUES ("walid"),("khaled"),("lazher");
+
+
+
+SELECT * FROM ninjas;
+INSERT INTO ninjas (firt_name,last_name,age)
+VALUES ("walid","benmousa","25"),("khaled","hannechi","30"),("lazher","arouch","27");
