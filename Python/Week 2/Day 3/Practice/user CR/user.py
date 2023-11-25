ccd9fe7e1ccb4a925f9c19fdcbf801f36cc3690d
nathing11 <amenallahbrahim6@gmail.com>
2023-11-25 18:38:57 +0100

    dz

---
+++ Python/Week 2/Day 3/Practice/user CR/user.py
@@ -0,0 +1,29 @@
+from mysqlconnection import connectToMySQL
+from pprint import pprint
+DATABAYSE="users_schema"
+class User:
+    def __init__( self , data ):
+        self.id = data['id']
+        self.first_name = data['firt_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.cretaed_at = data['cretaed_at']
+        self.updated_at = data['updated_at']
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM user;"
+        results = connectToMySQL(DATABAYSE).query_db(query)
+        users=[]
+        for row in results:
+            one_user = cls(row)
+            users.append(one_user)
+        return users
+    @classmethod
+    def gett_all(cls,data):
+        query="""
+                INSERT INTO user (firt_name,last_name,email) 
+                values (%(firt_name)s,%(last_name)s,%(email)s)
+                """
+        results = connectToMySQL(DATABAYSE).query_db(query,data)
+        return results
+        
