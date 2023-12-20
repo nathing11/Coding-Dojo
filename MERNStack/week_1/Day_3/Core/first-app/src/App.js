b27494ccfffd58dc14c6ed64038cdd3f16b28197
nathing11 <amenallahbrahim6@gmail.com>
2023-12-20 23:08:15 +0100

    aa

---
+++ MERNStack/week_1/Day_3/Core/first-app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo</h1>
+      <h2>Things  I need to do:</h2>
+      <ul>
+        <li>*learn react</li>
+        <li>*Climb mt everest</li>
+        <li>*run  a marathin</li>
+        <li>*feed the dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
